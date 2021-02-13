e7cf21020ec8211979e691e5f5c0e4aef5ffdf53
MatteoGisondi <matteo.gisondi@gmail.com>
2021-02-13 14:58:14 -0500

    Added basic skeleton for login

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, session, redirect, render_template, flash
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from forms import LoginForm, RegisterForm
+import bcrypt
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+# allow the use of @login_required on endpoints which require an account
+# unauthenticated users will be redirected to login page
+
+class User(UserMixin):
+    def __init__(self, first_name, last_name, username, email, password=None):
+        self.id = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return 'User({})'.format(self.username)
 
 
 @app.route('/')
@@ -11,7 +30,22 @@ def index():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = None  # TODO : check that user exists
+        password = None  # TODO : check that password matches
+        if user and password:
+            login_user(user)
+            flash('Logged in successfully.')
+            # check if the next page is set in the session by the @login_required decorator
+            # if not set, it will default to '/'
+            next_page = session.get('next', '/')
+            # reset the next page to default '/'
+            session['next'] = '/'
+            return redirect(next_page)
+        else:
+            flash('Incorrect username/password')
+    return render_template('login.html', form=form)
 
 
 @app.route('/create')
---
+++ forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import InputRequired, EqualTo, Email, Length
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Login')
+
+
+class RegisterForm(FlaskForm):
+    username_validator = ''  # TODO : regex for valid username (i.e. no special chars)
+    username = StringField('Username',
+                           validators=[InputRequired(),
+                                       Length(4, 32),
+                                       username_validator])
+    email = EmailField('Email', validators=[InputRequired(), Email()])
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired(), Length(8)])
+    password2 = PasswordField('Repeat password',
+                              validators=[InputRequired(), EqualTo('password', message='Passwords must match.')])
+    submit = SubmitField('Login')
+
+    def validate_password(self, password):
+        # provide password validation criteria
+        return True
