3f77447c344b44674bb391531815e7672bdd36a5
infinatrix <60296341+infinatrix@users.noreply.github.com>
2021-02-13 15:48:00 -0500

    Links to nav added, simple login box added

--- app.py
+++ app.py
@@ -7,6 +7,18 @@ app.secret_key = 'secret_key'
 @app.route('/')
 def index():
     return render_template('base.html')
+@app.route('/home')
+def home():
+    return render_template('base.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+    
 
 
 if __name__ == '__main__':
