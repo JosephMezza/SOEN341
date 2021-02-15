6f4cfcfabe77641c37e2ee5d494fc3b06925f4ef
steven-arsan <stevenarsan12@gmail.com>
2021-02-15 09:45:23 -0500

    'Added 5 users to the table'

--- database.py
+++ database.py
@@ -10,7 +10,16 @@ db = mysql.connector.connect(
 
 cr = db.cursor()
 
-#cr.execute("CREATE TABLE User_info (user VARCHAR(20), password VARCHAR(16), followers INT(255))")
+cr.execute("CREATE TABLE user_info (user VARCHAR(20), password VARCHAR(16), email VARCHAR(100), follower1 VARCHAR(20), follower2 VARCHAR(20), follower3 VARCHAR(20))")
 
+sql = "INSERT INTO user_info (user, password, email, follower1, follower2, follower3) VALUES (%s, %s, %s, %s, %s, %s)"
+val = [
+    ('Peter', 'Hello123', 'peter@hotmail.com', 'Johnny', 'Kevin', 'Jason'),
+    ('Jason', 'Password1', 'Jason.h@gmail.com', 'Johnny', 'Kevin', 'Richard'),
+    ('Johnny', 'notapassword', 'johnny.yespapa@yahoo.com', 'Kevin', 'Richard', 'Kate'),
+    ('Kevin', 'donthackme', 'Kevin.Durant@gmail.com', 'Richard', 'Kate', 'Peter'),
+    ('Kate', 'Hardpassword11', 'Katey@hotmail.com', 'Peter', 'Johnny', 'Jason')
+]
+cr.executemany(sql,val)
 
- 
+db.commit() 
