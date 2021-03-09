36b25af0091d119e6c53f1c06aedeff595b64c99
alexandru Bara <alexandrubara2000@gmail.com>
2021-03-09 11:18:01 -0500

    added posts calss with comment and addpost methods

---
+++ posts.py
@@ -0,0 +1,49 @@
+import time
+from datetime import date
+import csv
+def getListFromCSV(fileName):
+    dataList=[]
+    with open(fileName, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        dataList = list(reader)
+        return dataList
+
+def setListCSV(fileName, listToWrite):
+    with open(fileName, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        writer.writerows(listToWrite)
+
+
+
+class posts:
+    def __init__(self, user, imgpath):
+        self.likes = 0
+        self.user = user
+        self.imgpath = imgpath
+        today = date.today() 
+        self.time = today.strftime("%b-%d-%Y")
+        self.comments = []
+        postsList = getListFromCSV('data/posts.csv')
+        self.postID = len(postsList)-1
+
+
+
+def addPost(user, imgpath):
+    newPost = posts(user, imgpath)
+    postsList = getListFromCSV('data/posts.csv')
+    post = []
+    post.append(newPost.postID)
+    post.append(newPost.user)
+    post.append(newPost.imgpath)
+    post.append(newPost.likes)
+    post.append(newPost.time)
+    postsList.append(post)
+    setListCSV('data/posts.csv', postsList)
+
+def addComment(comment, postID):
+    postsList = getListFromCSV('data/posts.csv')
+    postsList[postID].append(comment)
+    setListCSV('data/posts.csv', postsList)
+
+addPost("Ablion73", "img(1).jpg")
+addComment("wow so ugly!", 1)
