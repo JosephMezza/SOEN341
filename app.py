206b417498e58e508cb93ca571b192923eb9fd5e
alexandru Bara <alexandrubara2000@gmail.com>
2021-04-10 10:49:45 -0400

    refractored the python files

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, session, redirect, render_template, flash, request, url_for
 from flask_login import LoginManager, login_user, logout_user, login_required
 from forms import LoginForm, SignUpForm, CaptionForm, ResetPasswordForm, EmailForm
-from re import I
-import bcrypt
+from werkzeug.utils import secure_filename
 from user import User
 from post import Post
+from re import I
+import bcrypt
 import tornado.web
 import tornado.ioloop
 import os
-from werkzeug.utils import secure_filename
 import mysql.connector
 import emailsend
 
@@ -24,8 +24,8 @@ except mysql.connector.errors.InterfaceError:
     db_config['host'] = '192.168.1.53'
     db = mysql.connector.connect(**db_config)
 finally:
-    print('Successfully connected to db {} on {} with user {}'.format(
-        db_config['database'], db_config['host'], db_config['user']))
+    print('Successfully connected to db {} on {} with user {}'.format(db_config['database'], db_config['host'], db_config['user']))
+
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
@@ -47,36 +47,26 @@ def load_user(user_id):
 
 @app.route('/')
 def index():
-    # print(session['username'])
-    # eventually change to logged in user
     username = "Calasts53"
     imageList = []
     try:
         username = str(session['_user_id'])
-        print(username)
         imageList = User.getImagesToShow(db, username)
-        imagedict = {imageList[index * 2 + 1]: imageList[index * 2]
-                     for index in len(imageList) / 2 - 5}
+        imagedict = {imageList[index * 2 + 1]: imageList[index * 2] for index in len(imageList) / 2 - 5}
         print(imagedict)
     except:
         print("An exception occurred")
         username = "Calasts53"
-    print(username)
-
-    # , loggedIn=session['loggedIn']
     return render_template('main.html', imageList=imageList)
 
-# Test User:
-# Calasts53
-# eeG1fior0g
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         try:
             user = User.getUserByUsername(db, form.username.data)
-            valid_password = bcrypt.checkpw(form.password.data.encode(
-                'utf-8'), user.password.encode('utf-8'))
+            valid_password = bcrypt.checkpw(form.password.data.encode('utf-8'), user.password.encode('utf-8'))
             if user and valid_password:
                 session['loggedIn'] = True
                 login_user(user)
@@ -110,15 +100,13 @@ def logout():
 def sign_up():
     """ TODO : This must be changed to searching the database"""
     form = SignUpForm()
-    print(form.username)
     if form.validate_on_submit():
         # check first if user already exists
         user = User.get_user(db, form.username.data)
         if not user:
             salt = bcrypt.gensalt()
             password = bcrypt.hashpw(form.password.data.encode('utf-8'), salt)
-            User.addUser(db, form.username.data, password.decode(
-            ), form.email.data, form.first_name.data, form.last_name.data)
+            User.addUser(db, form.username.data, password.decode(), form.email.data, form.first_name.data, form.last_name.data)
             flash('Sign up successful.')
             return redirect('/login')
         else:
@@ -137,7 +125,6 @@ def post(image):
         comment = request.form.get("comment")
         Post.addComment(db, comment, id)
         return redirect("/post/"+image)
-
     return render_template('post.html', id=id, postList=postList)
 
 
@@ -155,11 +142,8 @@ def users():
 
 @app.route('/profile/<username>', methods=["GET", "POST"])
 def profile(username):
-    print(username)
     imageList = User.imagesForUser(username)
-    print(imageList)
     likes = Post.getAllLikes(username)
-    print(likes)
     followers = User.getUserFollowers(username)
     following = User.getUserFollowing(username)
     return render_template('profile.html', imageList=imageList, username=username, likes=likes, followers=followers, following=following)
@@ -172,19 +156,15 @@ def postimage():
         try:
             if request.files:
                 image = request.files["image"]
-                print(image)
                 imageString = str(image)
                 indexOne = imageString.index('\'')
                 indexTwo = imageString.index('\'', indexOne+1)
                 imageName = imageString[indexOne+1:indexTwo]
-                print(imageName)
                 indexOfDot = imageName.index('.')
                 extensionName = imageName[indexOfDot+1::]
-                print(extensionName)
                 if extensionName.lower() == "png" or extensionName.lower() == "jpg" or extensionName.lower() == "gif":
                     image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
                     print("Image saved")
-                    # return redirect(request.url)
                     return redirect("/caption/" + imageName)
                 else:
                     return redirect("/upload-image")
@@ -211,7 +191,7 @@ def forgotPassword():
         email = form.email.data
         print(email)
         link = "http://127.0.0.1:5000/resetPassword/"+email
-        emailsend.sendemail(email, link)
+        emailsend.send_email(email, link)
         return redirect("/")
     return render_template('forgotPassword.html', form=form)
 
@@ -224,15 +204,11 @@ def ResetPassword(emailadress):
             # gets information of password from form
             password = form.password.data
             password2 = form.password2.data
-            print(password)
-            print(password2)
             # checks to see if password is good
             if password != password2:
                 return redirect("/resetPassword/"+emailadress)
-
             email = emailadress
-            print(email)
-            # changePassword(email, password) implementation needed in database
+            # changePassword(email, password) implementation needed in database*************
             return redirect("/")
     return render_template('resetPassword.html', form=form, emailadress=emailadress)
 
--- forms.py
+++ forms.py
@@ -13,27 +13,15 @@ class LoginForm(FlaskForm):
 class SignUpForm(FlaskForm):
 
     first_name = StringField('First Name', validators=[InputRequired(),])
-                                                 #       Regexp('^[a-zA-Z]+$', 0,
-                                                 #              'First Name must be alphabetic')
+    # Regexp('^[a-zA-Z]+$', 0,'First Name must be alphabetic')
     last_name = StringField('Last Name', validators=[InputRequired(),])
-                                                 #     Regexp('^[a-zA-Z]+$', 0,
-                                                 #            'Last Name must be alphabetic')
-    email = EmailField('Email', validators=[InputRequired(),
-                                            Email()])
-    username = StringField('Username',
-                           validators=[InputRequired(),
-                                       Length(4, 32),])
-                                   #     Regexp('^[a-zA-Z0-9._]+', 0,
-                                   #            'Username must be alphanumeric and can contain . and _')
-    password = PasswordField('Password',
-                             validators=[InputRequired(),
-                             Length(8),])
-                            #  Regexp('^[a-zA-Z0-9_!]+', 0,
-                            #         'Password must be alphanumeric and can contain ! and _')
-    password2 = PasswordField('Repeat password',
-                              validators=[InputRequired(),
-                                          EqualTo('password',
-                                                  message='Passwords must match')])
+    # Regexp('^[a-zA-Z]+$', 0,'Last Name must be alphabetic')
+    email = EmailField('Email', validators=[InputRequired(),Email()])
+    username = StringField('Username', validators=[InputRequired(), Length(4, 32),])
+    # Regexp('^[a-zA-Z0-9._]+', 0,'Username must be alphanumeric and can contain . and _')
+    password = PasswordField('Password', validators=[InputRequired(), Length(8),])
+    # Regexp('^[a-zA-Z0-9_!]+', 0,'Password must be alphanumeric and can contain ! and _')
+    password2 = PasswordField('Repeat password', validators=[InputRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Sign Up')
 
 
@@ -48,13 +36,7 @@ class EmailForm(FlaskForm):
 
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password',
-                             validators=[InputRequired(),
-                             Length(8),])
-                            #  Regexp('^[a-zA-Z0-9_!]+', 0,
-                            #         'Password must be alphanumeric and can contain ! and _')
-    password2 = PasswordField('Repeat password',
-                              validators=[InputRequired(),
-                                          EqualTo('password',
-                                                  message='Passwords must match')])
+    password = PasswordField('Password',validators=[InputRequired(), Length(8),])
+    # Regexp('^[a-zA-Z0-9_!]+', 0, 'Password must be alphanumeric and can contain ! and _')
+    password2 = PasswordField('Repeat password', validators=[InputRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Sign Up')
