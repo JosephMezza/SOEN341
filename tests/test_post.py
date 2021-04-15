d1a08973b6e9dc8127d11bc3deabf3c35b4e3a00
MatteoGisondi <matteo.gisondi@gmail.com>
2021-04-15 12:02:18 -0400

    Adjusted formatting

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,33 +1,43 @@
 from post import Post, Comment
 from user import User
-from base64 import b64encode
 import mysql.connector
-import datetime
-import os
+
+
 db = mysql.connector.connect(
     host='184.144.173.26',
     user='root',
     passwd='Binstagram_341',
     database='binstagram'
 )
+
 post = Post.get_by_id(db, 3)
 
-def test_get_user_likes(): 
-    assert(post.get_user_likes(db) == ['Ablion73', 'Whimseeplis', 'Marknow', 'Crinsonast1984'])
+
+def test_get_user_likes():
+    """compare a list of know users with those in db"""
+    assert(set(post.get_user_likes(db)) == (
+           'Ablion73', 'Whimseeplis', 'Marknow', 'Crinsonast1984'))
+
 
 def test_get_user():
+    """check if a user has posted a post"""
     user = User.get_from_db(db, 'id', 40)
     assert(post.get_user(db) == user)
 
+
 def test_get_post_comments():
-    post_comments = Comment.get_post_comments(db,post)
+    """check a known comment has been posted to a particular image"""
+    post_comments = Comment.get_post_comments(db, post)
     s1 = str(post_comments['alexabarra'])
-    assert( s1 == 'Nice Pic')
+    assert(s1 == 'Nice Pic')
+
+
 def test_like():
+    """tests like and unlike"""
     likes = len(post.get_user_likes(db))
     print(likes)
-    user = User.get_from_db(db,'id',3)
+    user = User.get_from_db(db, 'id', 3)
     print(user)
-    post.like(db,user)
-    post.unlike(db,user)
+    post.like(db, user)
+    post.unlike(db, user)
     assert(len(post.get_user_likes(db)) == likes)
